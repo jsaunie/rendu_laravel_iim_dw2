90a4db95f74ef98dd38a2bebf17c68d7fba0535f
Unknown <glrd.remi@gmail.com>
2018-01-21 16:35:09 +0100

    Relation Comment

--- blog/app/Comment.php
+++ blog/app/Comment.php
@@ -9,4 +9,13 @@ class Comment extends Model
     protected $table = 'comments';
     
     protected $guarded = [];
+    
+    public function getPost()
+    {
+        return $this->belongsTo(Post::class, 'id','fk_post_id');
+    }
+    public function getAuthor()
+    {
+        return $this->belongsTo(User::class, 'id', 'fk_user_id');
+    }
 }
