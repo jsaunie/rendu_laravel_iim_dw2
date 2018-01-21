018153a3ce07359ba83cf8e57929cecba6bb308c
Jean <jean.saunie@devinci.fr>
2018-01-21 23:26:10 +0100

    Auto stash before merge of "master" and "origin/master"

--- blog/resources/views/app/entities/posts/show.blade.php
+++ blog/resources/views/app/entities/posts/show.blade.php
@@ -27,6 +27,7 @@
                 <!-- Blog Post -->
                 <div class="card mb-4">
                     <img class="card-img-top" src="{{ asset('storage/' . $post->url_img ) }}" alt="Card image cap">
+
                     <div class="card-body">
                         <p class="card-text">{{ $post->content }}</p>
                     </div>
