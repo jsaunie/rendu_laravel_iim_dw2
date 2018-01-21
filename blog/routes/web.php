a1339587b996347327acadf0e293f5d1986fea2e
Unknown <glrd.remi@gmail.com>
2018-01-21 21:22:51 +0100

    fix route and img

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -21,7 +21,7 @@ Auth::routes();
 
 Route::group(['prefix' => 'blog'], function () {
     Route::get('/', ['uses' => 'App\BlogController@index']);
-    Route::get('/show{slug}', ['uses' => 'App\BlogController@show']);
+    Route::get('/show/{slug}', ['uses' => 'App\BlogController@show']);
     Route::get('/{slug}/vote', ['uses' => 'App\BlogController@vote']);
     Route::get('/create', ['uses' => 'App\BlogController@create']);
     Route::post('/create', ['uses' => 'App\BlogController@post']);
